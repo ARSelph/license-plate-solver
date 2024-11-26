816888dc5a00ad3f18510c52e7a468960ddc2d0e
Adam Selph <adam@selph.net>
2024-11-26 17:30:26 -0500

    basic license plate solver functionality

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,85 @@
-import Image from "next/image";
+'use client'
+import { useState, useEffect } from "react";
+
+interface TreeNode {
+  children: LetterMap,
+  value: string,
+  laterChars: Set<string>,
+  terminus: boolean
+}
+
+interface LetterMap {
+  [key: string]: TreeNode
+}
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const [tree, setTree] = useState<null | TreeNode>(null);
+  const [input, setInput] = useState('');
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+  useEffect(() => {
+    const createTree = async () => {
+      const words = await fetch("https://raw.githubusercontent.com/dwyl/english-words/refs/heads/master/words.txt")
+        .then(res => res.text())
+        .then(text => text.split('\n'));
+
+      const result: TreeNode = {
+        children: {},
+        value: '',
+        laterChars: new Set(),
+        terminus: false
+      }
+
+      // console.log('words', words);
+
+      words.forEach(word => {
+        if (word.includes('-')) return;
+        let current = result;
+        const letterArr = word.toLowerCase().split('')
+        letterArr.forEach((char, i) => {
+          letterArr.slice(i).forEach(letter => current.laterChars.add(letter));
+          if (!current.children[char]) {
+            current.children[char] = {
+              children: {},
+              value: char,
+              laterChars: new Set(),
+              terminus: false
+            }
+          }
+          current = current.children[char];
+          if (i === word.length - 1) current.terminus = true;
+        });
+      });
+      setTree(await result);
+    }
+    createTree();
+  }, []);
+
+  // console.log('tree', tree);
+
+  const calcWords = () => {
+    const candidates: string[] = [];
+    const findWord = (node: TreeNode, accumulated: string, remaining: string) => {
+      accumulated += node.value;
+      if (node.value === remaining[0]) remaining = remaining.slice(1);
+      if (node.terminus && remaining === '') {
+        candidates.push(accumulated);
+      }
+      const queue: TreeNode[] = [];
+      const remainingSet = new Set(remaining.split(''));
+      Object.values(node.children).forEach(child => {
+        if (remainingSet.isSubsetOf(child.laterChars)) queue.push(child);
+      })
+      queue.forEach(child => findWord(child, accumulated, remaining));
+    }
+    findWord(tree as TreeNode, '', input);
+    candidates.sort((a, b) => a.length < b.length ? -1 : 1);
+    console.log('candidates', candidates);
+  }
+
+  return (
+    <div className="">
+      <input className="m-3 border-box" type="text" value={input} onChange={e => setInput(e.target.value)}></input>
+      <button onClick={calcWords}>Submit</button>
     </div>
   );
 }
